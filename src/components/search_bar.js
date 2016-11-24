3f5414cc078ff2296cdd102b4067892fda9ee57d
Adriana Matsufuji <adrianabm@gmail.com>
2016-11-24 16:47:16 +0100

    SearchBar Component

---
+++ src/components/search_bar.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+
+class SearchBar extends Component {
+  render() {
+    return (
+      <input onChange={ this.onInputChange } />
+    )
+  }
+
+  // eventHandler function
+  onInputChange() {
+
+  }
+
+}
+
+export default SearchBar
--- src/index.js
+++ src/index.js
@@ -1,15 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
+import React from 'react'
+import ReactDOM from 'react-dom'
 
-import App from './components/app';
-import reducers from './reducers';
+import SearchBar from './components/search_bar'
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
+const API_KEY = 'AIzaSyBEPTtXyhw7NCufGqSlR0f28eO37KpwoSY'
 
-ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
-  </Provider>
-  , document.querySelector('.container'));
+// Create a new component. This component should produce some HTML.
+const App = () => {
+  return (
+    <div>
+      <SearchBar />
+    </div>
+  )
+}
+
+// Take this component's generated HTML and put it on the page (in the DOM)
+ReactDOM.render(<App />, document.querySelector('.container'))
