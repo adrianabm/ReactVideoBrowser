a97b14179650fae2464f4258727c801f75efa114
Adriana Matsufuji <adrianabm@gmail.com>
2016-11-25 11:59:57 +0100

    Create video list and video details components

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React, { Component } from 'react'
 import ReactDOM from 'react-dom'
 import YTSearch from 'youtube-api-search'
-
 import SearchBar from './components/search_bar'
+import VideoList from './components/video_list'
 
 const API_KEY = 'AIzaSyBEPTtXyhw7NCufGqSlR0f28eO37KpwoSY'
 
@@ -24,6 +24,7 @@ class App extends Component {
     return (
       <div>
         <SearchBar />
+        <VideoList videos={ this.state.videos } />
       </div>
     )
   }
