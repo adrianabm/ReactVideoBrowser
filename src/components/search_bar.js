c40fcf90e779b1ba7577ae8304e7d103b6128404
Adriana Matsufuji <adrianabm@gmail.com>
2016-11-25 09:23:07 +0100

    Changed SearchBar component and youtube search with API

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -1,17 +1,24 @@
 import React, { Component } from 'react'
 
 class SearchBar extends Component {
+  // This is how we initialize state in class-based components. Functional components do not have state.
+  constructor(props) {
+    super(props)
+
+    this.state = { term: '' }
+  }
+
   render() {
     return (
-      <input onChange={ this.onInputChange } />
+      <div>
+        <input
+          // controlled component: has its value set by state; it's value only changes when the state changes
+          value = { this.state.term }
+          onChange={ event => this.setState({ term: event.target.value }) } />
+
+      </div>
     )
   }
-
-  // eventHandler function
-  onInputChange() {
-
-  }
-
 }
 
 export default SearchBar
--- src/index.js
+++ src/index.js
@@ -1,9 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import YTSearch from 'youtube-api-search'
 
 import SearchBar from './components/search_bar'
 
 const API_KEY = 'AIzaSyBEPTtXyhw7NCufGqSlR0f28eO37KpwoSY'
+
+YTSearch({ key: API_KEY, term: 'surfboards'}, function(data) {
+  console.log(data)
+})
 
 // Create a new component. This component should produce some HTML.
 const App = () => {
