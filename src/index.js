95be16a9b733d5bd0ab36ec22c78b02842b5dc96
Adriana Matsufuji <adrianabm@gmail.com>
2016-11-25 15:26:34 +0100

    Select video

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom'
 import YTSearch from 'youtube-api-search'
 import SearchBar from './components/search_bar'
 import VideoList from './components/video_list'
+import VideoDetail from './components/video_detail'
 
 const API_KEY = 'AIzaSyBEPTtXyhw7NCufGqSlR0f28eO37KpwoSY'
 
@@ -10,13 +11,17 @@ class App extends Component {
   constructor(props) {
     super(props)
 
-    this.state = { videos: [] }
+    this.state = {
+      videos: [],
+      selectedVideo: null
+     }
 
     // Starting with a random YT search so the page won't be empty when first rendered
     YTSearch({ key: API_KEY, term: 'surfboards'}, (videos) => {
-      this.setState({ videos })
-      // this.setState({ videos: videos })
-      // When key and value are the same name, it's possible to shorten the syntax
+      this.setState({
+        videos: videos,
+        selectedVideo: videos[0]
+       })
     })
   }
 
@@ -24,7 +29,11 @@ class App extends Component {
     return (
       <div>
         <SearchBar />
-        <VideoList videos={ this.state.videos } />
+        <VideoDetail video={ this.state.selectedVideo } />
+        <VideoList
+          onVideoSelect={ selectedVideo => this.setState({ selectedVideo }) }
+          videos={ this.state.videos }
+        />
       </div>
     )
   }
